729b4d03dbdc83fb1bfd19fb403ec9a3a2afc293
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-08 12:16:02 +0530

    First approach to use multiple Reducer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
     return {
@@ -9,17 +10,27 @@ function buyCake() {
         info: 'First redux action'
     }
 }
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
 
 //(previousState, action)=> newState
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams:20
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
         }
         default: return state
     }
@@ -33,4 +44,6 @@ console.log('Updated State', store.getState())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
